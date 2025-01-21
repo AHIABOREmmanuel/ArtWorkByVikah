84e751bab73850fd8348a56a50500911ea8fc10f
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-01-21 21:37:16 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './body/resume/resume.component';
+import { ProjetsComponent } from './body/projets/projets.component';
+import { ContactComponent } from './body/contact/contact.component';
+import { BodyComponent } from './body/body.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'accueil',component:BodyComponent},
+  {path:'resume',component:ResumeComponent},
+  {path:'projets',component:ProjetsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'',component:BodyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResumeComponent } from './body/resume/resume.component';
+import { ProjetsComponent } from './body/projets/projets.component';
+import { ContactComponent } from './body/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BodyComponent,
+    FooterComponent,
+    ResumeComponent,
+    ProjetsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
